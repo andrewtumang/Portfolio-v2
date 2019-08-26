1a44db7586b37237176c13cb1335b986b11c36c3
atumang <30819059+atumang@users.noreply.github.com>
2019-08-26 13:32:51 -0700

    About Theme1 Response

--- js/about1.js
+++ js/about1.js
@@ -1,3 +1,9 @@
 $(".test").click(function() {
   $("body").css("background-color",sessionStorage.bg);
 })
+
+$(window).resize(function () {
+  if($(window).width() >= 1100) {
+    window.scrollTo(0,0);
+  }
+})
